967f88dc4033bb7150d8761dd28001cbcc782c42
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-12-22 17:47:39 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,2 +1,2 @@
-$( ".fr.co > img.fl" ).replaceWith('<img src="https://wlllh.com/flag/sa.gif" style="width:16px;border-radius:1px;" src="flag/sa.gif">');
-$("img[src='prv1.png?2']").attr("src","https://wlllh.com/flag/sa.gif"); 
+$( ".fr.co > img.fr" ).replaceWith('<img src="https://wlllh.com/flag/sa.gif" style="width:16px;border-radius:1px;" src="flag/sa.gif">');
+$("img[src='flag/ye.gif']").attr("src","https://wlllh.com/flag/sa.gif"); 
