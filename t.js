3869a3b9ed3ae77ccfeb37650fa1946533605821
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-12-22 21:49:34 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,2 +1,1 @@
-$( ".fr.co > img.alt" ).replaceWith('<img src="" style="width:16px;border-radius:1px;" src="flag/sa.gif">');
 $(`<img[alt=""='flag/ye.gif']").attr ("src","https://wlllh.com/flag/sa.gif"class="fr co" style="width:16px;border-radius:1px;">`);
