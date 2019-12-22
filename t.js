8001d0b5ed1968d09eb8d57df420790d334cc3a4
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-12-22 17:39:49 +0300

    Create t.js

---
+++ t.js
@@ -0,0 +1,2 @@
+$( ".fr.co > img.fl" ).replaceWith('<img src="https://wlllh.com/flag/sa.gif" style="width:16px;border-radius:1px;" src="flag/sa.gif">');
+$("img[src='prv1.png?2']").attr("src","https://wlllh.com/flag/sa.gif"); 
