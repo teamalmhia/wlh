c658b20a9600de40c0f808efe1335658918236a6
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-12-23 23:16:33 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,1 +1,1 @@
-$(`<img style="width:16px;border-radius:1px;margin-top: -80px;margin-left: 315px" src="flag/sa.gif" border="0">`).insertAfter('.fr.co');
+$(`<img style="width:16px;border-radius:1px;margin-top: -80px;margin-left: 315px" src="https://wlllh.com/flag/sa.gif" border="0">`).insertAfter('.fr.co');
