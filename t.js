c21d2323628d442dfc48c60d22126f5ed0a21c1b
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-12-23 20:48:46 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,2 +1,1 @@
-$(`<img alt="" class="fr co" style="width:16px;border-radius:1px;" src="https://wlllh.com/flag/sa.gif">`).insertBefore('.fr.co');
-$(`<img style="width:18px;border-radius:1px;margin: -18px" src="flag/sa.gif" border="0">`).insertAfter('.fr.co');
+
