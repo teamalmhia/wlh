0ad9e61c6a7c2f5fb571fe483fa935fc431b8215
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-12-23 19:42:22 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,3 +1,1 @@
-$(`<img[alt=""='flag/ye.gif']").attr ("src","https://wlllh.com/flag/sa.gif"class="fr co" style="width:16px;border-radius:1px;">`);
-$( ".hand.nosel.fl.uzr.border> img.fr.co" ).replaceWith('<img src="https://wlllh.com/flag/sa.gif" style="margin-right: 2px; width: 25px;">');
-$(`<img alt="" class="fr co" style="width:16px;border-radius:1px;" src="flag/sa.gif">`).insertBefore('.fr.co');
+$(`<img alt="" class="fr co" style="width:16px;border-radius:1px;" src="https://wlllh.com/flag/sa.gif">`).insertBefore('.fr.co');
